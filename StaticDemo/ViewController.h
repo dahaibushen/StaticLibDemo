32eb4c8fab57c07b38c5f2ccede14ed71aff40cb
hu <>
2019-04-20 13:44:16 +0800

    pod库添加

---
+++ StaticDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticDemo
+//
+//  Created by hu on 2019/4/20.
+//  Copyright © 2019 huyiyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StaticDemo/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  StaticDemo
+//
+//  Created by hu on 2019/4/20.
+//  Copyright © 2019 huyiyong. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <HYYLogic/HYYLogic.h>
+#import <HYYCommon/HYYCommon.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
